a60e6a8bc93320257fc93420cd83c1517a2eaa4f
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-11 15:31:51 +0700

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🐷+😣*'
+if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🙁+😣*'
    	let [emoji1, emoji2] = text.split`+`
 		let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
                  for (let res of anu.results) {
