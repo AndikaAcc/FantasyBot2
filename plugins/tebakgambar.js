01bf930231c5977ea7f2865b8c9580d38b743ec7
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-12 07:31:21 +0700

    Create tebakgambar.js

---
+++ plugins/tebakgambar.js
@@ -0,0 +1,36 @@
+let fetch = require('node-fetch')
+
+let timeout = 180000
+let poin = 5000
+let handler = async (m, { conn, usedPrefix }) => {
+  conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
+  let id = m.chat
+  if (id in conn.tebakgambar) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgambar[id][0])
+    throw false
+  }
+  let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakgambar.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+  // if (!json.status) throw json
+  let caption = `
+  ${json.deskripsi}
+⏲️Timeout *${(timeout / 1000).toFixed(2)} detik*
+📝Ketik ${usedPrefix}hint untuk hint
+⭐Bonus: ${poin} XP
+🔖 Jawab dengan reply pesan ini
+    `.trim()
+  conn.tebakgambar[id] = [
+    await conn.sendFile(m.chat, json.img, 'tebakgambar.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
+    json, poin,
+    setTimeout(() => {
+      if (conn.tebakgambar[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakgambar[id][0])
+      delete conn.tebakgambar[id]
+    }, timeout)
+  ]
+}
+handler.help = ['tebakgambar']
+handler.tags = ['game']
+handler.command = /^tebakgambar/i
+handler.limit = true
+
+module.exports = handler
