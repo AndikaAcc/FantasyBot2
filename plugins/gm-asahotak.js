519192579839ade9c9d74a20d958ac8673b28fb5
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-13 05:08:47 +0700

    Update gm-asahotak.js

--- plugins/gm-asahotak.js
+++ plugins/gm-asahotak.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 let timeout = 120000
-let poin = 500
+let poin = 200
 let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
