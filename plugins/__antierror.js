96ddde1f78667c8972f6b2e7617a42579a650345
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-12 13:16:14 +0700

    Update __antierror.js

--- plugins/__antierror.js
+++ plugins/__antierror.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.all = async function (m) {
     let user = global.db.data.users[m.sender]
     if ((user.money * 1) > 99999998) {
-        user.money = 99999999
+        user.money = 999999999999999
     } else if ((user.money * 1) < 0) {
         user.money = 0
     }
